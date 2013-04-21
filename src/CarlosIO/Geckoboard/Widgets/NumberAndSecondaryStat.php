0a730a17bb500d8b1a21967693022f04fe2ed74b
Carlos Buenosvinos <hi@carlos.io>
2013-04-21 13:00:15 +0200

    #4 Fix Enhancement Request for Number & 2nd Stat Widget

--- src/CarlosIO/Geckoboard/Widgets/NumberAndSecondaryStat.php
+++ src/CarlosIO/Geckoboard/Widgets/NumberAndSecondaryStat.php
@@ -3,12 +3,22 @@ namespace CarlosIO\Geckoboard\Widgets;
 
 use CarlosIO\Geckoboard\Widgets\Widget;
 
+/**
+ * Class NumberAndSecondaryStat
+ * @package CarlosIO\Geckoboard\Widgets
+ */
 class NumberAndSecondaryStat extends Widget
 {
     private $mainValue = null;
     private $secondaryValue = null;
     private $mainPrefix = null;
 
+    /**
+     * Set data main prefix (€, $, etc.)
+     *
+     * @param string $mainPrefix
+     * @return $this
+     */
     public function setMainPrefix($mainPrefix)
     {
         $this->mainPrefix = $mainPrefix;
@@ -16,11 +26,22 @@ class NumberAndSecondaryStat extends Widget
         return $this;
     }
 
+    /**
+     * Get data main prefix (€, $, etc.)
+     *
+     * @return string
+     */
     public function getMainPrefix()
     {
         return $this->mainPrefix;
     }
 
+    /**
+     * Set main value
+     *
+     * @param int $mainValue
+     * @return $this
+     */
     public function setMainValue($mainValue)
     {
         $this->mainValue = $mainValue;
@@ -28,11 +49,22 @@ class NumberAndSecondaryStat extends Widget
         return $this;
     }
 
+    /**
+     * Get main value
+     *
+     * @return int
+     */
     public function getMainValue()
     {
         return $this->mainValue;
     }
 
+    /**
+     * Set secondary value
+     *
+     * @param int $secondaryValue
+     * @return $this
+     */
     public function setSecondaryValue($secondaryValue)
     {
         $this->secondaryValue = $secondaryValue;
@@ -40,25 +72,41 @@ class NumberAndSecondaryStat extends Widget
         return $this;
     }
 
+    /**
+     * Get secondary value
+     *
+     * @return string
+     */
     public function getSecondaryValue()
     {
         return $this->secondaryValue;
     }
 
+    /**
+     * Get data in array format
+     *
+     * @return array
+     */
     public function getData()
     {
-        return array(
+        $result = array(
             'item' => array(
                 array(
                     'text' => '',
                     'value' => (int) $this->getMainValue(),
                     'prefix' => (string) $this->getMainPrefix()
-                ),
-                array(
-                    'text' => '',
-                    'value' => (int) $this->getSecondaryValue()
                 )
             )
         );
+
+        $secondaryValue = $this->getSecondaryValue();
+        if (null !== $secondaryValue) {
+            $result['item'][] = array(
+                'text' => '',
+                'value' => (int) $secondaryValue
+            );
+        }
+
+        return $result;
     }
 }
