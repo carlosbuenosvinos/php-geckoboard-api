4693b0f0069afef69de6e9cf53243406e49cac9c
Otto Odgaard <oodgaard@ultraserve.com.au>
2014-12-08 14:38:05 +1000

    Label can now be null for ItemList widget.

--- src/CarlosIO/Geckoboard/Widgets/ItemList.php
+++ src/CarlosIO/Geckoboard/Widgets/ItemList.php
@@ -9,11 +9,15 @@ class ItemList extends Widget
 {
     protected $items = array();
 
-    public function addItem(Title $title, Label $label, $description)
+    public function addItem(Title $title, $label, $description)
     {
         $count = count($this->items);
         $this->items[$count]['title'] = $title->toArray();
-        $this->items[$count]['label'] = $label->toArray();
+
+        if ($label instanceof Label) {
+            $this->items[$count]['label'] = $label->toArray();
+        }
+
         $this->items[$count]['description'] = $description;
     }
 
